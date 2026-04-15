e0c3082af46fbf50ff76dbce3806242180f50132
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-15 22:12:56 +0100

    Implement grouping, correlation and summary analysis

--- pandas-data-wrangling/main.py
+++ pandas-data-wrangling/main.py
@@ -15,9 +15,31 @@ df.loc[df["Duration"] > 120, "Duration"] = 120
 # remove duplicates
 df = df.drop_duplicates()
 
-# remove invalid dates
+# remove invalid values
 df = df.dropna()
 
+# reset index (for cleaner output)
+df = df.reset_index(drop=True)
+
+# Data Exploration
+
+print("\n--- CLEAN DATA ---\n")
 print(df.to_string())
+
+print("\n--- INFO ---\n")
 print(df.info())
+
+print("\n--- DESCRIBE ---\n")
 print(df.describe())
+
+print("\n--- MEAN BY CATEGORY ---\n")
+print(df.groupby("Category")[["Price", "Duration"]].mean())
+
+print("\n--- VALUE COUNTS (Category) ---\n")
+print(df["Category"].value_counts())
+
+print("\n--- MOST EXPENSIVE PRODUCT ---\n")
+print(df.loc[df["Price"].idxmax()])
+
+print("\n--- CORRELATION ---\n")
+print(df[["Duration", "Price"]].corr())
