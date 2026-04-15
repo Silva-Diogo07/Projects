f4b11a1d9edad04d3a3ddbf5d3d01b4d0fb79401
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-15 21:16:55 +0100

    fixed wrong path problem

--- pandas-data-wrangling/main.py
+++ pandas-data-wrangling/main.py
@@ -2,7 +2,8 @@ import pandas as pd
 from pathlib import Path
 
 script_dir = Path(__file__).parent
-dataCSV_path = script_dir / "data" / "sales_data.csv"
+main_dir = script_dir.parent    
+dataCSV_path = main_dir / "data" / "sales_data.csv"
 
 df = pd.DataFrame(dataCSV_path)
 print(df)
