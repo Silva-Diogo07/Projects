ac5152079846078ce84777fa0bc81e29cc5a3c9c
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-15 21:20:13 +0100

    now actually finished problem (me being dumb and not reading the file)

--- pandas-data-wrangling/main.py
+++ pandas-data-wrangling/main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-script_dir = Path(__file__).parent
-main_dir = script_dir.parent    
-dataCSV_path = main_dir / "data" / "sales_data.csv"
+script_dir = Path(__file__).parent  
+dataCSV_path = script_dir / "data" / "sales_data.csv"
 
-df = pd.DataFrame(dataCSV_path)
-print(df)
+df = pd.read_csv(dataCSV_path)
+print(df.to_string())
