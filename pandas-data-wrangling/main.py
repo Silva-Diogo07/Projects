5554d7160f55a68eec1454d991ccb131fcfa4d78
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-15 22:06:46 +0100

    first version of dataset cleaning (dates, duplicates, filtering)

--- pandas-data-wrangling/main.py
+++ pandas-data-wrangling/main.py
@@ -5,4 +5,19 @@ script_dir = Path(__file__).parent
 dataCSV_path = script_dir / "data" / "sales_data.csv"
 
 df = pd.read_csv(dataCSV_path)
+
+# convert dates properly
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+
+# limit Duration
+df.loc[df["Duration"] > 120, "Duration"] = 120
+
+# remove duplicates
+df = df.drop_duplicates()
+
+# remove invalid dates
+df = df.dropna()
+
 print(df.to_string())
+print(df.info())
+print(df.describe())
