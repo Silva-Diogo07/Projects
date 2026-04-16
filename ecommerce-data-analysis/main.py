522395785af368fc472cd7b37cd87722db00e7c7
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-16 15:10:35 +0100

    data grouping

--- ecommerce-data-analysis/main.py
+++ ecommerce-data-analysis/main.py
@@ -6,8 +6,18 @@ dataCSV_path = script_dir / "data" / "e-commerce_orders.csv"
 
 # read csv
 df = pd.read_csv(dataCSV_path)
-
 df = df.drop_duplicates()
-df = df.reset_index(drop=True)
 
-print(df)
+# new 'total' column
+
+df["total"] = df["quantity"] * df["price"]
+
+# order by country and price
+df_sorted = df.sort_values(["country", "price"], ascending=[True, True])
+
+# Total by country and cleaner output
+totals = df.groupby("country")["total"].sum().reset_index()
+
+print(df_sorted)
+print("\nTotal by country:")
+print(totals)
