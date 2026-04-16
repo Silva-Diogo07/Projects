455dfe0b8920be97a3f5d2b97b86746145b74ecf
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-16 19:20:07 +0100

    find the customer that spent the most and his country

--- customer-spending-analytics/main.py
+++ customer-spending-analytics/main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 script_dir = Path(__file__).parent
 dataCSV_path = script_dir / "data" / "customer_order.csv"
-
 df = pd.read_csv(dataCSV_path)
 
 # create total column
@@ -16,3 +15,11 @@ df = df.sort_values(["country", "order_date"])
 for country, data in df.groupby("country"):
     print(f"\nCountry: {country}")
     print(data.to_string(index=False))
+
+# find the customer who spent the most and his country
+total_by_customer = df.groupby("customer_id")["total"].sum()
+top_customer = total_by_customer.idxmax()
+top_value = total_by_customer.max()
+top_country = df[df["customer_id"] == top_customer]["country"].iloc[0]
+
+print(f"Top customer: {top_customer}\nTotal spent: {top_value:,.2f}€\nCustomer country: {top_country}")
