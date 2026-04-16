68bcc3d2ff27e7f8a6bf0dc5cf78ed7dd4682def
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-16 14:38:46 +0100

    remove duplicates

--- ecommerce-data-analysis/main.py
+++ ecommerce-data-analysis/main.py
@@ -6,5 +6,8 @@ dataCSV_path = script_dir / "data" / "e-commerce_orders.csv"
 
 # read csv
 df = pd.read_csv(dataCSV_path)
-print(df)
 
+df = df.drop_duplicates()
+df = df.reset_index(drop=True)
+
+print(df)
