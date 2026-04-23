43b6f25550eccf40747b734e28881aa5f1e20652
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-04-23 21:18:34 +0100

    customers that spent the most and the least

--- customer-retail-analysis/main.py
+++ customer-retail-analysis/main.py
@@ -11,14 +11,26 @@ df = df.sort_values(by=["country", "customer_id"]).groupby("country").head(5)
 
 print(df)
 
-# finds the youngest customer
+# finds the youngest customer in each country
 youngest_customers = df.loc[df.groupby("country")["age"].idxmin()]
 print("\nYoungest customers by country:")
 for _, row in youngest_customers.iterrows():
     print(f"From {row['country']} is the customer {row['customer_id']} with the age of {row['age']} ")
 
-# finds the oldest customer
-oldest_customer = df.loc[df.groupby("country")["age"].idxmax()]
+# finds the oldest customer in each country
+oldest_customers = df.loc[df.groupby("country")["age"].idxmax()]
 print("\nOldest customer by country:")
-for _, row in oldest_customer.iterrows():
+for _, row in oldest_customers.iterrows():
     print(f"From {row['country']} is the customer {row['customer_id']} with the age of {row['age']} ")
+
+# finds the customers that spent the least in each country
+least_spent_customer = df.loc[df.groupby("country")["total_spent"].idxmin()]
+print("\nCustomers that spent the least:")
+for _, row in least_spent_customer.iterrows():
+    print(f"From {row['country']} is the customer {row['customer_id']} with {row['purchases']} purchases with a total of {row['total_spent']}£ spent")
+
+# finds the customers that spent the most in each country
+top_spent_customers = df.loc[df.groupby("country")["total_spent"].idxmax()]
+print("\nCustomers that spent the most:")
+for _, row in top_spent_customers.iterrows():
+    print(f"From {row['country']} is the customer {row['customer_id']} with {row['purchases']} purchases with a total of {row['total_spent']}£ spent")
